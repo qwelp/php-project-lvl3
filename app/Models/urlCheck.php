f1937811f352de1b0446b3dc6a546ca5aa0505e8
Vladimir Bun <qwelp@mail.ru>
2022-05-31 14:53:54 +0300

    Fix

--- app/Models/urlCheck.php
+++ app/Models/urlCheck.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class urlCheck extends Model
+class UrlCheck extends Model
 {
     use HasFactory;
 }
