887d54237ee483ad4bc4496f5f7896d5e935768c
Vladimir Bun <qwelp@mail.ru>
2022-06-22 12:17:29 +0300

    Fix

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,12 @@ use App\Http\Controllers\UrlCheckController;
 use App\Http\Controllers\UrlController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',
+Route::get(
+    '/',
     function () {
         return view('welcome');
     }
 )->name('home');
 
-Route::resource('urls',UrlController::class)->only('index', 'store', 'show');
+Route::resource('urls', UrlController::class)->only('index', 'store', 'show');
 Route::resource('urls.checks', UrlCheckController::class)->only('store');
